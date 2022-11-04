81875f18846d1d950bdbdaaa0767397fa244ea39
brycerobinsn <bryce.robinson@gmail.com>
2022-11-03 22:56:25 -0700

    create new flights

---
+++ controllers/flights.js
@@ -0,0 +1,24 @@
+const Flight = require('../models/flight')
+
+module.exports = {
+    // index,
+    new: newFlight,
+    create
+}
+
+// function index(req, res) {
+//     Flight.find({}, function(err, flights){
+//         res.render('flights/index', {title: 'All Flights', flights})
+//     })
+// }
+function create(req, res){
+    const flight = new Flight(req.body)
+    flight.save(function(err){
+        if (err) return res.redirect('/flights/new')
+        console.log(flight)
+        res.redirect('/flights')
+    })
+}
+function newFlight (req, res) {
+    res.render('flights/new')
+}
---
+++ routes/flights.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// router.get('/', flightsCtrl.index)
+router.get('/new', flightsCtrl.new)
+router.post('/', flightsCtrl.create)
+
+module.exports = router;
