92a83563d9f5fee2b43c01d931be0550026b1900
brycerobinsn <bryce.robinson@gmail.com>
2022-11-03 21:31:58 -0700

    npm i mongoose add flight model

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 // connect to database with mongoose
-require('/config/database')
+require('./config/database')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
